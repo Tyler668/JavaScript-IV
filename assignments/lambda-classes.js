c79a6fdbc0683b447871337470ed8e0ab1d2e692
Tyler Gilchrist <tyler668@gmail.com>
2019-07-25 15:19:16 -0400

    Created instructor and student class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -6,13 +6,41 @@ class Person{
         this.name = attrs.name,
         this.location = attrs.location,
         this.age = attrs.age
-        // this.favLaguage = attrs.favLaguage,
-        // this.catchPhrase = attrs.catchPhrase,
-        //this.specialty = attrs.specialty
     }
-
     speak(){
         console.log(`Hello, my name is ${this.name}, I am from ${this.location}`);
     }
 }
 
+
+
+class Instructor extends Person{
+    constructor(attrs){
+        super(attrs);
+        this.specialty = attrs.specialty;
+        this.favLaguage = attrs.favLaguage;
+        this.catchPhrase = attrs.catchPhrase
+    }
+    demo(subject){
+        console.log(`Today, we will be learning about ${subject}`);
+    }
+    grade(studentObj, subject){
+        console.log(`${studentObj.name} receives a perfect score on ${subject}`);
+    }
+}
+
+class Student extends Person{
+    constructor(attrs){
+        super(attrs);
+        this.previousBackground = attrs.previousBackground;
+        this.className = attrs.className;
+        this.favSubjects = attrs.favSubjects;
+    }
+        listsSubjects(){
+            for(i = 0; i< this.favSubjects.length; i++){
+                console.log(i + '. ' + this.favSubjects[i]);
+            }
+        
+    }
+
+}
